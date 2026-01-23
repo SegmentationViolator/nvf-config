816929663dbdf1fff5c5d1491006bcd6e5252c1b
Zackie <102979057+MasterZack69@users.noreply.github.com>
2026-01-23 10:32:42 +0530

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
     config.vim = {
         utility.images.image-nvim = {
             enable = true;
-            setupOpts.backend = "kitty";
+            setupOpts.backend = "sixel";
         #   setupOpts.editorOnlyRenderWhenFocused = true;
         };
         autocomplete.blink-cmp = {
