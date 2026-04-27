c676b3d170cfc641d971f26eda0822ec35be0514
Segmentation Violator <segmentationviolator@proton.me>
2026-04-27 13:03:03 +0530

    fix: use blink.cmp

--- modules/nvf/workspace.nix
+++ modules/nvf/workspace.nix
@@ -1,6 +1,21 @@
 _: {
     flake.modules.nvf.default = _: {
         config.vim = {
+            autocomplete.blink-cmp = {
+                enable = true;
+                setupOpts = {
+                    cmdline.keymap.preset = "super-tab";
+
+                    keymap."<C-k>" = [
+                        "show_signature"
+                        "hide_signature"
+                        "fallback"
+                    ];
+
+                    signature.enabled = true;
+                };
+            };
+
             diagnostics = {
                 enable = true;
                 config = {
